31f19c01088f4bfd389c22d47fcf6a9bac82b9a5
nodename <alan@nodename.com>
2021-08-23 17:34:58 -0400

    Added dev/user.clj for lein repl

---
+++ dev/user.clj
@@ -0,0 +1,9 @@
+(ns user
+  (:require [clojure.pprint :refer (pprint)]
+            [clojure.repl :refer :all]
+            [clojure.test :as test]
+            ;[clojure.tools.namespace.repl :refer (refresh refresh-all)]
+            [mate-clj.core :refer [d-> dreduce]]))
+
+
+;(println "user ns loaded")
